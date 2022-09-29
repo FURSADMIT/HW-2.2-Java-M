2ebf984201daac0dc21caf174c10efdef9b34131
Fursa <fursad14@mail.ru>
2022-09-29 23:12:18 +0300

    Main.java changed

--- Main.java
+++ Main.java
@@ -4,14 +4,10 @@ public class Main {
     public static void main(String[] args) {
 
         int account = 200;
-        int transfer = 5000;
+        int transfer = 1_000;
 
-        int bonus = transfer / 100;
-        if (transfer > 1000) {
-            bonus = transfer / 100;
-        } else {
-            bonus = 0;
-        }
+        int bonus;
+        bonus = 0;
         System.out.println("Счет: " + (account + transfer + bonus) + " рублей");
         for (String s : Arrays.asList("Сумма пополнения: " + transfer + " рублей", "Бонусные рубли: " + bonus + " рублей")) {
             System.out.println(s);
